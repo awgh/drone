4bb9865d019b4551f8c2bba8dcb812c8b23fea40
awgh <awgh@awgh.org>
2016-10-24 15:19:23 -0700

    First attempt at making the drone remotely reconfigurable.

--- drone/main.go
+++ drone/main.go
@@ -9,9 +9,11 @@ import (
 
 	// Must underscore include any Keypairs, Routers, Policies, or Transports you want compiled in
 	_ "github.com/awgh/bencrypt/ecc"
+	_ "github.com/awgh/bencrypt/rsa"
 	_ "github.com/awgh/ratnet/policy"
 	_ "github.com/awgh/ratnet/router"
 	_ "github.com/awgh/ratnet/transports/https"
+	_ "github.com/awgh/ratnet/transports/udp"
 )
 
 func main() {
@@ -20,22 +22,31 @@ func main() {
 	flag.StringVar(&configFile, "c", "config.json", "JSON Config File")
 	flag.Parse()
 
+	// Load initial configuration from file
 	content, err := ioutil.ReadFile(configFile)
-	if err != nil {
-		log.Println(err)
-	}
-	node := ram.New(nil, nil)
-	err = node.Import(content)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("Starting...\n\n", string(content))
+	node := ram.New(nil, nil)
+	if err := node.Import(content); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Starting...\n\n", string(content)+"\n")
 	node.Start()
 
 	//go func() {
 	for {
 		msg := <-node.Out()
-		log.Println(msg)
+		nodeNew := ram.New(nil, nil)
+		content := msg.Content.Bytes()
+		if err := nodeNew.Import(content); err == nil {
+			node.Stop()
+			node = nodeNew
+			log.Println("Restarting...\n\n", string(content)+"\n")
+			node.Start()
+		} else {
+			log.Println("Import failed:\n", string(content)+"\n")
+		}
 	}
 	//}()
 }
